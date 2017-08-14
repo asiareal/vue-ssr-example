dd5c020a9965100ba92897c6c32f5ca549a104d3
wangyazhou <wangyazhou@tuniu.com>
2017-08-14 14:22:32 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# vue ssr 服务器端渲染例子
+
+> 最近在学习vue ssr，将学习过程做的一些例子做了一些整理，汇集于此
+
+## demo1
+
+```bash
+$ npm run dev:demo1
+```
+ssr与服务器集成的基本例子，对应[官方教程](https://ssr.vuejs.org/zh/basic.html)
---
+++ demo1/server.js
@@ -0,0 +1,33 @@
+// 请参照：https://ssr.vuejs.org/zh/basic.html
+// 服务端渲染基本用法
+
+const Vue = require('vue')
+const server = require('express')()
+const fs = require('fs')
+const renderer = require('vue-server-renderer').createRenderer({
+  template: fs.readFileSync('./demo1/index.template.html', 'utf-8')
+})
+const context = {
+  title: 'hello',
+  meta: `
+    <meta charset="UTF-8">
+  `
+}
+server.get('*', (req, res) => {
+  // 新建vue对象
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>访问的 URL 是： {{ url }}</div>`
+  })
+  // 将vue对象渲染成字符串，插到指定地方 <!--vue-ssr-outlet-->
+  renderer.renderToString(app, context, (err, html) => {
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+    res.end(html)
+  })
+})
+server.listen(8080)
